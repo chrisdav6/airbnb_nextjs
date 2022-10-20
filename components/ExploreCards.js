1e6c228b5664f90c196b582000e82e323c8f118d
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2022-10-20 15:32:29 -0400

    Completed ExploreCards component

--- components/ExploreCards.js
+++ components/ExploreCards.js
@@ -2,12 +2,14 @@ import SmallCard from './SmallCard';
 
 const ExploreCards = ({ exploreData }) => {
   return (
-    <section className='pt-6'>
+    <section className='pt-8'>
       <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
 
-      {exploreData?.map((item, idx) => (
-        <SmallCard key={idx} item={item} />
-      ))}
+      <div className='grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+        {exploreData?.map((item, idx) => (
+          <SmallCard key={idx} item={item} />
+        ))}
+      </div>
     </section>
   );
 };
